cad839023398aa4311acc2fcef1de97745999ff6
Sergey Denisov <sergdenisov@ozon.ru>
2026-02-27 23:29:01 +0300

    wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './app/product/products.module';
 import { CategoryModule } from './app/category/category.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductModule, CategoryModule],
+  imports: [ProductModule, CategoryModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
