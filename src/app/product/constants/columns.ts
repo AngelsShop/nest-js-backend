1734b423ee77e192c45a9926557637b0c303ddaa
Sergey Denisov <sergdenisov@ozon.ru>
2026-02-28 21:17:01 +0300

    Добавляет работу с БД для продуктов

---
+++ src/app/product/constants/columns.ts
@@ -0,0 +1,9 @@
+export enum ColumnsMap {
+  id = 'id',
+  name = 'name',
+  description = 'description',
+  price = 'price',
+  image = 'image',
+  size = 'size',
+  categoryId = 'category_id',
+}
--- src/app/product/types/product.ts
+++ src/app/product/types/product.ts
@@ -1,15 +1,23 @@
+import { Size } from 'src/constants/size';
+import { PageDataRequest } from 'src/types/PageData';
+
 export type Product = {
   id: string;
   name: string;
   description: string;
   price: number;
   image: string;
+  size: Size;
 };
 
 export type CreateProduct = Omit<Product, 'id'>;
 
+export type ListProductsRequest = {
+  filter: ListProductsFilter;
+  pageData: PageDataRequest;
+};
+
 export type ListProductsFilter = {
-  page: number;
-  limit: number;
   categoryId?: string;
+  size?: Size;
 };
